010d06af775944b5276a46bd830ecb5144e48ba9
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-03-05 22:11:18 -0300

    Create routes and router

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,1 +1,19 @@
-console.log("hi");
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("Viewing dashboard")},
+        { path: "/posts", view: () => console.log("Viewing posts")},
+        { path: "/settings", view: () => console.log("Viewing settings")}
+    ]
+
+    // Test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route,
+            isMatch: location.pathname === route.path
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    router()
+})
