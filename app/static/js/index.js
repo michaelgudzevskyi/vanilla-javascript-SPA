445faa906c29c7ce6ca17067e78c88028e5fb5eb
Michael Gudzevskyi <hello@michaelgudzevskyi.com>
2021-03-06 10:12:09 -0300

    Create browser history

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,3 +1,8 @@
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+}
+
 const router = async () => {
     const routes = [
         { path: "/", view: () => console.log("Viewing dashboard")},
@@ -21,8 +26,19 @@ const router = async () => {
             isMatch: true
         }
     }
+
+    console.log(match.route.view());
 }
 
+window.addEventListener("popstate", router)
+
 document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")){
+            e.preventDefault()
+            navigateTo(e.target.href)
+        }
+    })
+    
     router()
 })
